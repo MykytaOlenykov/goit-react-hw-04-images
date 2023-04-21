ddb4ecf59a134adab8c25ea9de5205f2996d9ea9
ForestDr <nikita2001395@gmail.com>
2023-04-21 18:54:25 +0300

    add hooks in App component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { Component, createRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import { GlobalStyle } from 'components/GlobalStyle';
 import * as imgsAPI from 'services';
@@ -8,89 +8,19 @@ import { Loader } from 'components/Loader';
 import { ImageGallery } from 'components/ImageGallery';
 import * as S from './App.styled';
 
-export class App extends Component {
-  state = {
-    searchQuery: '',
-    images: [],
-    currentPage: 1,
-    isLoading: false,
-    total: 0,
-  };
+export const App = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [total, setTotal] = useState(0);
+  const galleryRef = useRef(null);
 
-  galleryRef = createRef();
+  const isVisibleBtn =
+    !isLoading && images.length !== 0 && images.length < total;
 
-  getSnapshotBeforeUpdate(_, prevState) {
-    if (prevState.images.length < this.state.images.length) {
-      const gallery = this.galleryRef.current;
-
-      return gallery.scrollHeight - gallery.scrollTop;
-    }
-
-    return null;
-  }
-
-  componentDidUpdate(_, prevState, snapshot) {
-    const prevSearchQuery = prevState.searchQuery;
-    const nextSearchQuery = this.state.searchQuery;
-    const prevPage = prevState.currentPage;
-    const nextPage = this.state.currentPage;
-
-    if (snapshot !== null) {
-      window.scrollTo({ top: snapshot, behavior: 'smooth' });
-    }
-
-    if (prevSearchQuery !== nextSearchQuery || prevPage !== nextPage) {
-      this.fetchImgs();
-    }
-  }
-
-  handleSubmitForm = searchQuery => {
-    const isValid = this.validationSearchQuery(searchQuery);
-
-    if (isValid) {
-      this.setState({ searchQuery, currentPage: 1, images: [], total: 0 });
-    }
-  };
-
-  handleLoadMore = () => {
-    this.setState(({ currentPage }) => ({ currentPage: currentPage + 1 }));
-  };
-
-  async fetchImgs() {
-    const { searchQuery, currentPage } = this.state;
-    const options = { searchQuery, currentPage };
-
-    try {
-      this.setState({ isLoading: true });
-
-      const { hits, totalHits } = await imgsAPI.getImgs(options);
-
-      if (currentPage === 1) {
-        if (!hits.length) {
-          toast.error(`No results found for ${searchQuery}`);
-          return;
-        }
-
-        this.setState({ images: hits, total: totalHits });
-      } else {
-        this.setState(({ images }) => ({
-          images: [...images, ...hits],
-        }));
-      }
-
-      this.checkIsAllCollection({
-        collectionSize: hits.length,
-        total: totalHits,
-      });
-    } catch (error) {
-      console.log(error);
-    } finally {
-      this.setState({ isLoading: false });
-    }
-  }
-
-  validationSearchQuery(newSearchQuery) {
-    if (newSearchQuery === this.state.searchQuery) {
+  const validationSearchQuery = newSearchQuery => {
+    if (newSearchQuery === searchQuery) {
       toast.info(
         `Request for ${newSearchQuery} already processed. Enter new value.`
       );
@@ -99,30 +29,78 @@ export class App extends Component {
     }
 
     return true;
-  }
+  };
 
-  checkIsAllCollection({ collectionSize, total }) {
-    if (collectionSize >= total) {
-      toast.success(
-        `You have uploaded all images for request ${this.state.searchQuery}`
-      );
+  const handleSubmitForm = searchQuery => {
+    const isValid = validationSearchQuery(searchQuery);
+
+    if (isValid) {
+      setSearchQuery(searchQuery);
+      setCurrentPage(1);
+      setImages([]);
+      setTotal(0);
     }
-  }
+  };
 
-  render() {
-    const { images, isLoading, total } = this.state;
-    const isVisibleBtn =
-      !isLoading && images.length !== 0 && images.length < total;
+  const handleLoadMore = () => {
+    setCurrentPage(prevState => prevState + 1);
+  };
 
-    return (
-      <S.Container>
-        <GlobalStyle />
-        <Searchbar onSubmit={this.handleSubmitForm} />
-        <ImageGallery ref={this.galleryRef} images={images} />
-        {isVisibleBtn && <Button onLoadMore={this.handleLoadMore} />}
-        {isLoading && <Loader />}
-        <ToastContainer autoClose={5000} />
-      </S.Container>
-    );
-  }
-}
+  useEffect(() => {
+    if (searchQuery === '') {
+      return;
+    }
+
+    const checkIsAllCollection = ({ collectionSize, total }) => {
+      if (collectionSize >= total) {
+        toast.success(
+          `You have uploaded all images for request ${searchQuery}`
+        );
+      }
+    };
+
+    const fetchImgs = async () => {
+      const options = { searchQuery, currentPage };
+
+      try {
+        setIsLoading(true);
+
+        const { hits, totalHits } = await imgsAPI.getImgs(options);
+
+        if (currentPage === 1) {
+          if (!hits.length) {
+            toast.error(`No results found for ${searchQuery}`);
+            return;
+          }
+
+          setImages(hits);
+          setTotal(totalHits);
+        } else {
+          setImages(prevState => [...prevState, ...hits]);
+        }
+
+        checkIsAllCollection({
+          collectionSize: hits.length,
+          total: totalHits,
+        });
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchImgs();
+  }, [searchQuery, currentPage]);
+
+  return (
+    <S.Container>
+      <GlobalStyle />
+      <Searchbar onSubmit={handleSubmitForm} />
+      <ImageGallery ref={galleryRef} images={images} />
+      {isVisibleBtn && <Button onLoadMore={handleLoadMore} />}
+      {isLoading && <Loader />}
+      <ToastContainer autoClose={5000} />
+    </S.Container>
+  );
+};
